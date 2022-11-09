817830690f6565187b9b876f8bb7ef2c4c245365
shalva96 <79251572+shalva96@users.noreply.github.com>
2022-11-09 17:25:06 +0400

    done observe for fragments

--- app/src/main/java/com/example/todolist/DataClass/DataModel.kt
+++ app/src/main/java/com/example/todolist/DataClass/DataModel.kt
@@ -3,6 +3,8 @@ package com.example.todolist.DataClass
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+// Model for fragments
+
 open class DataModel: ViewModel() {
 
     val openFragEmptyFragment: MutableLiveData<Boolean> by lazy {
